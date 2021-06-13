39943e64fa535264fb86b3165b5e9b4a14aff20e
Iwandoin <51027505+Iwandoin@users.noreply.github.com>
2021-06-14 00:56:03 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -138,7 +138,7 @@ class App extends Component {
                   className="btn btn-primary"
                   onClick={this.createItem}
                 >
-                  Add taaaaaaask
+                  Add taaaaaaassssssk
                 </button>
               </div>
               {this.renderTabList()}
